2074bdb34c48671c9ba7dd48fd368f6cc5b4eff4
unknown <oachkaa1121@gmail.com>
2026-02-09 23:07:36 +0800

    update

--- src/App.tsx
+++ src/App.tsx
@@ -1,30 +1,6 @@
-import {
-  Card,
-  CardAction,
-  CardContent,
-  CardDescription,
-  CardFooter,
-  CardHeader,
-  CardTitle,
-} from "@/components/ui/card";
-import "./App.css";
-
+import { NavigationMnu } from "./modules/home/components/NavigationMenu";
 function App() {
-  return (
-    <Card>
-      <CardHeader>
-        <CardTitle>Card Title</CardTitle>
-        <CardDescription>Card Description</CardDescription>
-        <CardAction>Card Action</CardAction>
-      </CardHeader>
-      <CardContent>
-        <p>Card Content</p>
-      </CardContent>
-      <CardFooter>
-        <p>Card Footer</p>
-      </CardFooter>
-    </Card>
-  );
+  return <NavigationMnu />;
 }
 
 export default App;
